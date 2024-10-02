09a980fde31e25ca2cc924d3a536201c035060ae
Petr Fifka <petr.fifka@tredgate.cz>
2024-10-02 20:53:29 +0200

    First JS scripts

---
+++ javascript-learning/first-scripts/values.js
@@ -0,0 +1,5 @@
+console.log("Petr");
+console.log(15);
+
+console.log(15 + 15);
+console.log("15" + "15");
---
+++ javascript-learning/first-scripts/variables.js
@@ -0,0 +1,45 @@
+let firstName = "Petr";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+firstName = "Alžběta";
+
+console.log(firstName);
+
+firstName = "Radek";
+
+console.log(firstName);
+
+var promennaVar;
+var promennaVar = "haha!";
+
+promennaVar = "(var) Jsem naplněná na samostatném řádku";
+{
+  var x = "jsem var";
+}
+
+console.log(x);
+
+let promennaLet;
+promennaLet = "Jsem naplněná na samostatném řádku";
+{
+  let y = "jsem let";
+}
+
+// console.log(y);
+
+const promennaConst = "Jsem constant";
+
+{
+  const z = "jsem blok const";
+}
+
+// console.log(z);
+let number = 1;
+let boolean = true;
+let falseBoolean = false;
